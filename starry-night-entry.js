4b6c6e72b409f438c4441692470789bdb655a857
Joen Tolgraven <joen.tolgraven@gmail.com>
2025-07-28 01:56:08 +0200

    package.json: add shadow as prod dep as well

---
+++ starry-night-entry.js
@@ -0,0 +1,6 @@
+import { createStarryNight } from '@wooorm/starry-night';
+
+// Export starry-night functions to global scope for ClojureScript to access
+export { createStarryNight };
+
+console.log('Starry-night bundle loaded successfully');
