ad454afcdbdf52f00e012652858eb490e8b62547
Joen Tolgraven <joen@tolgraven.se>
2026-09-09 19:29:17 +0200

    build: reconcile React tooling and restrict deployment to master

--- project.clj
+++ project.clj
@@ -2,22 +2,22 @@
   :description "tolgrAVen.se website"
   :url "https://tolgraven.se"
 
-  :dependencies [[thheller/shadow-cljs "3.2.1"]
+  :dependencies [[thheller/shadow-cljs "3.3.2"]
                  [thheller/shadow-util "0.7.0"]
                  [thheller/shadow-client "1.4.0"]
                  [thheller/shadow-undertow "0.3.4"]
                  [thheller/shadow-cljsjs "0.0.22"]
 
                  ; all of shadows deps, explicit
-                 [org.clojure/clojure "1.12.2"]
+                 [org.clojure/clojure "1.12.3"]
                  [org.clojure/data.json "2.5.1"]
-                 [org.clojure/tools.cli "1.1.230"]
+                 [org.clojure/tools.cli "1.2.245"]
                  [org.clojure/tools.reader "1.5.2"]
-                 [nrepl "1.3.1"]
+                 [nrepl "1.5.1"]
                  [com.cognitect/transit-clj "1.0.333"]
                  [com.cognitect/transit-cljs "0.8.280"]
                  [org.clojure/core.async "1.8.741"]
-                 [org.clojure/clojurescript "1.12.42"]
+                 [org.clojure/clojurescript "1.12.116"]
                  [com.google.javascript/closure-compiler-unshaded "v20250820"]
                  [org.clojure/google-closure-library "0.0-20250515-87401eb8"]
                  [org.clojure/google-closure-library-third-party "0.0-20250515-87401eb8"]
@@ -35,7 +35,7 @@
                  [org.postgresql/postgresql "42.7.8"]
                  [honeysql "1.0.461"]
                  ; [com.google.guava/guava "25.1-jre"] ;what is
-                 [cprop "0.1.20"] ;env loading
+                 [cprop "0.1.21"] ;env loading
                  ; [expound "0.9.0"] ;spec failure explainers
                  [funcool/struct "1.4.0"] ;is what?
                  [clj-http "3.13.1"]
@@ -48,19 +48,19 @@
 
                  [metosin/jsonista "0.3.13"] ;also json en/decoding. supposedly faster. [cheshire "5.10.0"] ;json decoding
                  [metosin/muuntaja "0.6.11"]
-                 [metosin/reitit "0.9.1" :exclusions [org.clojure/spec.alpha com.fasterxml.jackson.core/jackson-core com.fasterxml.jackson.core/jackson-databind com.fasterxml.jackson.core/jackson-annotations]]
+                 [metosin/reitit "0.9.2" :exclusions [org.clojure/spec.alpha com.fasterxml.jackson.core/jackson-core com.fasterxml.jackson.core/jackson-databind com.fasterxml.jackson.core/jackson-annotations]]
 
                  [mount "0.1.23"]
 
                  ; LOGGING
                  [org.clojure/tools.logging "1.3.0"]
                  [com.taoensso/timbre "6.8.0" :exclusions [org.clojure/tools.reader]]
-                 [com.taoensso/encore "3.154.0"]
+                 [com.taoensso/encore "3.158.0"]
                  [com.fzakaria/slf4j-timbre "0.4.1" :exclusions [org.slf4j/slf4j-api]] ;route java logging through timbre. but dunno. hard to manage
                  [clj-logging-config "1.9.12"]
                  [io.aviso/pretty "1.4.4"] ;pretty exceptions, pretty logging...
 
-                 [ring/ring-core "1.15.2"]
+                 [ring/ring-core "1.15.3"]
                  [ring/ring-defaults "0.7.0" :exclusions [commons-io]]
                  [amalloy/ring-gzip-middleware "0.1.4"]
                  [ring-partial-content "2.1.0"] ; handle safari video playback / 206 response
@@ -101,9 +101,7 @@
                  ; [com.smxemail/re-frame-document-fx "0.0.1-SNAPSHOT"] ;https://github.com/SMX-LTD/re-frame-document-fx
                  [com.degel/re-frame-firebase "0.10.0-SNAPSHOT" :exclusions [args4j]]
                  ; [com.degel/re-frame-firebase "0.8.0" :exclusions [args4j cljsjs/firebase]]
-                 [reagent "1.3.0"]
-                 [cljsjs/react "18.3.1-1"]
-                 [cljsjs/react-dom "18.3.1-1"]
+                 [reagent "2.0.1"]
                  [reanimated "0.6.1"]
                  [com.smxemail/re-frame-cookie-fx "0.0.2"  :exclusions [args4j]]
                  [re-pollsive "0.1.0"] ; periodic polling/timer stuff
@@ -135,7 +133,7 @@
   {:language :clojurescript
    :output-path "resources/docs/codox"
    :metadata {:doc/format :markdown}
-   :namespaces [#"^(?!.*code\.cljs$).*\.cljs$"]
+   :namespaces [#"^(?!.*(code|ui)\.cljs$).*\.cljs$"] ; TODO ignore macro ns?
    :source-uri "https://github.com/tolgraven/tolgraven/blob/master/{filepath}#L{line}"}
 
   :profiles
@@ -151,11 +149,9 @@
                                  [pjstadig/humane-test-output "0.11.0"]
                                  [prone "2021-04-23"]
                                  [re-frisk "1.7.1"]
-                                 ; [re-frisk-remote "1.5.2"]
                                  [day8.re-frame/re-frame-10x "1.10.1" :exclusions [superstructor/re-highlight]]
-                                 ;[djblue/portal "0.61.0"]
                                  ; [day8.re-frame/tracing "0.6.2"]
-                                 [ring/ring-devel "1.15.2"]
+                                 [ring/ring-devel "1.15.3"]
                                  [ring/ring-mock "0.6.2"]]
                   :plugins      [;[refactor-nrepl/refactor-nrepl "3.9.0"]
                                  [cider/cider-nrepl "0.57.0"] ]
@@ -167,9 +163,12 @@
                                  :init-ns user
                                  :port 7000
                                  :init (start)
-                                 :nrepl-middleware [shadow.cljs.devtools.server.nrepl/middleware]
-                                 :timeout 30000} }
-   :project/test {:jvm-opts ["-Dconf=test-config.edn"]
+                                 :nrepl-middleware [shadow.cljs.devtools.server.nrepl/middleware
+                                                    #_cider.nrepl/cider-middleware]
+                                 :timeout 30000}
+                  }
+   :project/test {:source-paths ["test/cljs"]
+                  :jvm-opts ["-Dconf=test-config.edn"]
                   :resource-paths ["env/test/resources"]
                   :cljsbuild
                   {:builds
@@ -183,7 +182,6 @@
 
    
    :uberjar {:jvm-opts ["-Dconf=env/prod/resources/config.edn"]
-             :local-repo "/root/.m2"
              :plugins [[lein-shell "0.5.0"]]
              :prep-tasks ["compile"
                           ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]
@@ -192,6 +190,9 @@
              :uberjar-name "tolgraven.jar"
              :source-paths ["env/prod/clj" "env/prod/cljs"]
              :resource-paths ["env/prod/resources" "resources"]}
+
+   :prod {:source-paths ["env/prod/clj" "env/prod/cljs"]
+          :resource-paths ["env/prod/resources"]}
 
    :profiles/dev {}
    :profiles/test {}
--- src/cljs/tolgraven/react.cljs
+++ src/cljs/tolgraven/react.cljs
@@ -4,7 +4,6 @@
    [react-dom :as react-dom]
    [react :as react]
    [reagent.core :as r]
-   [reagent.dom]
    [reagent.ratom]))
 
 (def adapt-react-class         reagent.core/adapt-react-class)
@@ -14,14 +13,11 @@
 (def children                  reagent.core/children)
 (def create-class              reagent.core/create-class)
 (def cursor                    reagent.core/cursor)
-(def dom-node                  reagent.dom/dom-node)
 (def lazy                      react/lazy)
 (def reactify-component        reagent.core/reactify-component)
-(def render                    reagent.dom/render)
 (def make-reaction             reagent.ratom/make-reaction)
 (def track                     reagent.core/track)
 (def track!                    reagent.core/track!)
 (def create-portal             react-dom/createPortal)
 (def suspense                  (adapt-react-class react/Suspense))
-(def unmount-component-at-node reagent.dom/unmount-component-at-node)
 
